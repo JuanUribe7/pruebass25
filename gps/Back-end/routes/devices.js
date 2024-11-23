fe032157224c581e1aba3d8b10e5221739d0dbba
Diego <diegolara7820@gmail.com>
2024-11-23 02:25:22 -0500

    correcion

--- gps/Back-end/routes/devices.js
+++ gps/Back-end/routes/devices.js
@@ -16,28 +16,6 @@ router.get('/', async (req, res) => {
         res.status(500).json({ error: 'Error al obtener dispositivos: ' + error.message });
     }
 });
-router.post('/save-route', async (req, res) => {
-    try {
-      const { name, waypoints } = req.body;
-      const newRoute = new Route({ name, waypoints });
-      const savedRoute = await newRoute.save();
-      res.status(201).json(savedRoute);
-    } catch (error) {
-      console.error('Error al guardar la ruta:', error.message);
-      res.status(500).json({ error: 'Error al guardar la ruta: ' + error.message });
-    }
-  });
-  
-  // Endpoint para obtener una ruta
-  router.get('/get-route/:id', async (req, res) => {
-    try {
-      const route = await Route.findById(req.params.id);
-      res.json(route);
-    } catch (error) {
-      console.error('Error al obtener la ruta:', error.message);
-      res.status(500).json({ error: 'Error al obtener la ruta: ' + error.message });
-    }
-  });
 
 
 router.post('/save-history', async (req, res) => {
