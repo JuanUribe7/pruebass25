b6fee029583a263eed05f81dd0f34f5d57c56e2c
JuanUribe7 <tu_email@ejemplo.com>
2024-11-24 11:47:45 -0500

    asdsf

--- gps/src/components/NotificacionN.vue
+++ gps/src/components/NotificacionN.vue
@@ -21,10 +21,10 @@
             <div v-if="showMenu" class="menu-panel">
                 <div class="menu-header">Opciones</div>
                 <div class="menu-items">
-                    <div @click="clearNotifications" class="menu-item">
+                    <router-link to="#" @click="clearNotifications" class="menu-item">
                         <i class='bx bx-trash'></i>
                         <span>Limpiar todo</span>
-                    </div>
+                    </router-link>
                     <router-link to="/reporte2" class="menu-item">
                         <i class='bx bx-folder-open'></i>
                         <span>Ver historial</span>
