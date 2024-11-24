c757534ae91517ab8f8d9331d7f9020a43632165
JuanUribe7 <tu_email@ejemplo.com>
2024-11-24 12:21:25 -0500

    yuyh

--- gps/Back-end/utils/formatearFecha.js
+++ gps/Back-end/utils/formatearFecha.js
@@ -1,15 +1,15 @@
 export function formatDate(dateString) {
     const date = new Date(dateString);
 
-    // Obtener componentes de la fecha
-    const year = date.getFullYear();
-    const month = String(date.getMonth() + 1).padStart(2, '0'); // Meses empiezan desde 0
-    const day = String(date.getDate()).padStart(2, '0');
+    // Obtener componentes de la fecha en UTC
+    const year = date.getUTCFullYear();
+    const month = String(date.getUTCMonth() + 1).padStart(2, '0'); // Meses empiezan desde 0
+    const day = String(date.getUTCDate()).padStart(2, '0');
 
-    // Obtener componentes de la hora
-    let hours = date.getHours();
-    const minutes = String(date.getMinutes()).padStart(2, '0');
-    const seconds = String(date.getSeconds()).padStart(2, '0');
+    // Obtener componentes de la hora en UTC
+    let hours = date.getUTCHours();
+    const minutes = String(date.getUTCMinutes()).padStart(2, '0');
+    const seconds = String(date.getUTCSeconds()).padStart(2, '0');
 
     // Formatear AM/PM
     const ampm = hours >= 12 ? 'PM' : 'AM';
